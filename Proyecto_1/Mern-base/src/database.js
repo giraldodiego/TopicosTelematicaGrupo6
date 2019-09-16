eee7b20ff84d45a5e7e8ab7010df25fbf399d54b
Diego Giraldo <diegoagg95@hotmail.com>
2019-09-16 16:55:38 -0500

    K8S config

--- Proyecto_1/Mern-base/src/database.js
+++ Proyecto_1/Mern-base/src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //const URI ='mongodb://localhost/mern-post'
-const URI = 'mongodb://mongo-server/ProyectMERN';
+const URI = process.env.MONGO_URI;
 
 //sudo fuser -k 3000/tcp para error 
 
